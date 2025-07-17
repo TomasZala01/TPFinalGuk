2724ed37162e759588d9bd9c5a382ddf1c58746c
TomasZala01 <zala.m.tomas@gmail.com>
2025-07-17 08:38:19 -0300

    Update carrito.js

--- carrito.js
+++ carrito.js
@@ -87,5 +87,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const botonCompra = document.getElementById('comprar-btn');
 botonCompra.addEventListener('click', () => {
-    window.location.href = '/'; // Redirige al home ( inicioo)
+    window.location.href = '/index.html'; // Redirige al home ( inicioo)
 });
